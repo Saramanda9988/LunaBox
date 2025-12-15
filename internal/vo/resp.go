91659b9fe75b16caf2a4ee2b23a6fc0dc5a1f442
Saramanda9988 <2074730050@qq.com>
2025-12-16 00:22:16 +0800

    refactor: rename Response.go to response.go and add game statistics data structures

--- internal/vo/resp.go
+++ internal/vo/resp.go
@@ -1,6 +1,32 @@
 package vo
 
-import "lunabox/internal/enums"
+import (
+	"lunabox/internal/enums"
+	"lunabox/internal/models"
+	"time"
+)
+
+type CategoryVO struct {
+	ID        string    `json:"id"`
+	UserID    string    `json:"user_id"`
+	Name      string    `json:"name"`
+	IsSystem  bool      `json:"is_system"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	GameCount int       `json:"game_count"`
+}
+
+type GameMetadataFromWebVO struct {
+	Source enums.SourceType
+	Game   models.Game
+}
+
+type HomePageData struct {
+	RecentGames       []models.Game `json:"recent_games"`
+	RecentlyAdded     []models.Game `json:"recently_added"`
+	TodayPlayTimeSec  int           `json:"today_play_time_sec"`
+	WeeklyPlayTimeSec int           `json:"weekly_play_time_sec"`
+}
 
 type DailyPlayTime struct {
 	Date     string `json:"date"`     // YYYY-MM-DD
