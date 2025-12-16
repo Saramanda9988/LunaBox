219b3b8f9d800d0e733f65c1db5e65cf150b0c86
Saramanda9988 <2074730050@qq.com>
2025-12-16 19:25:37 +0800

    feat: add custom scrollbar styles and integrate CSS into main application

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client'
 import App from './App'
 import '@unocss/reset/tailwind.css'
 import 'virtual:uno.css'
+import './style.css'
 
 const container = document.getElementById('root')
 
