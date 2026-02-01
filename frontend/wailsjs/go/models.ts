f2a3e1bc26092e318034218779f0b404725c843d
Saramanda9988 <2074730050@qq.com>
2026-02-02 00:07:38 +0800

    feat: enhanced chart display

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -109,12 +109,6 @@ export namespace appconf {
 
 export namespace enums {
 	
-	export enum SourceType {
-	    LOCAL = "local",
-	    BANGUMI = "bangumi",
-	    VNDB = "vndb",
-	    YMGAL = "ymgal",
-	}
 	export enum Period {
 	    DAY = "day",
 	    WEEK = "week",
@@ -131,6 +125,12 @@ export namespace enums {
 	    PLAYING = "playing",
 	    COMPLETED = "completed",
 	    ON_HOLD = "on_hold",
+	}
+	export enum SourceType {
+	    LOCAL = "local",
+	    BANGUMI = "bangumi",
+	    VNDB = "vndb",
+	    YMGAL = "ymgal",
 	}
 
 }
