74a3f84ae7f910af6e2747fb56f777ec68872c25
Saramanda9988 <2074730050@qq.com>
2026-02-21 10:41:24 +0800

    feat: integrate i18next for internationalization support and add language detection

---
+++ frontend/src/i18n.ts
@@ -0,0 +1,24 @@
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
+import enUS from "./locales/en-US.json";
+import zhCN from "./locales/zh-CN.json";
+
+const resources = {
+  "zh-CN": { translation: zhCN },
+  "en-US": { translation: enUS },
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: "zh-CN",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
